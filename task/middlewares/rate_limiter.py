11c9b8ee5869649b7cb2c78e2f6de91747f68025
MisaghMomeniB <jenabemisagh@gmail.com>
2026-02-02 11:05:13 -0800

    reformat, hot fix

--- task/middlewares/rate_limiter.py
+++ task/middlewares/rate_limiter.py
@@ -3,17 +3,18 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from task.services.rate_limit_service import rate_limiter
 
+
 class RateLimitMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        
+    async def dispatch(self, request: Request, call_next):
+
         if request.url.path == "/ping":
-            client_ip = request.client.host
-            
+            client_ip = request.client.host if request.client else "anonymous"
+
             if not rate_limiter.is_allowed(client_ip):
                 return JSONResponse(
                     status_code=429,
                     content={"detail": "Rate limit exceeded"}
                 )
-                
-            response = await call_next(request)
-            return response
+
+        response = await call_next(request)
+        return response
